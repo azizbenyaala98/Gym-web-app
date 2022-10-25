579e09b87b9cf64277ad09ed3133ea1f36061d50
baha chammakhi <bahachammakhi25@gmail.com>
2022-10-25 12:18:14 +0000

    fix: keywords issues , modules

--- server/index.js
+++ server/index.js
@@ -1,31 +1,37 @@
-import express from "express";
-import { ApolloServer, gql } from "apollo-server-express";
-import typeDefs from "./typeDefs/typeDefs.js";
-import resolvers from "./resolvers/resolvers.js";
-import mongoose from "mongoose";
-import cors from "cors";
-import dotenv from "dotenv";
+import express from 'express';
+import { ApolloServer } from 'apollo-server-express';
+import typeDefs from './typeDefs/typeDefs.js';
+import resolvers from './resolvers/resolvers.js';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+async function connect() {
+  try {
+    mongoose.connect(process.env.mongodb);
+    mongoose.connection.on('connected', () => {
+      console.log('Connection established to the DB');
+    });
+    mongoose.connection.on('error', () => {
+      throw new Error(`unable to connect to database: ${config.db}`);
+    });
+  } catch (e) {
+    console.error(e);
+  }
+}
 
 async function initServer() {
   const app = express();
   app.use(cors());
   dotenv.config();
+  connect();
   const apolloServer = new ApolloServer({ typeDefs, resolvers });
   await apolloServer.start();
   apolloServer.applyMiddleware({ app });
   app.use((req, res) => {
-    res.send("server started successfully");
+    res.send('server started successfully');
   });
   const PORT = process.env.PORT || 5000;
-  try {
-    await mongoose.connect(process.env.mongodb);
-    console.log(`connected to mongodb at port ${PORT} `);
-  } catch (error) {
-    console.log(error);
-  }
-
-  app.listen(PORT, () =>
-    console.log(`Expresss server is running on port ${PORT}`)
-  );
+  app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
 }
 initServer();
